bf988a8afd01bad467938219ae2d41362bf4996f
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-11-08 22:05:08 +0000

    Fix: Multi-set bounds, version tracking, and double-free on merge
    `lower_bound` skipped equal keys in a multi-set, the AVL tree tracked the wrong visible version
    after an update, and `merge` freed the same node twice.
    
    Adds a tree-printing routine, which is how the shapes behind these three were read.

--- include/consistent_set/consistent_avl.hpp
+++ include/consistent_set/consistent_avl.hpp
@@ -67,6 +67,15 @@ class avl_node_gt {
         callback(node);
     }
 
+    template <typename callback_at>
+    static void for_each_left_right(node_t* node, callback_at&& callback) noexcept {
+        if (!node)
+            return;
+        for_each_left_right(node->left, callback);
+        callback(node);
+        for_each_left_right(node->right, callback);
+    }
+
     static node_t* find_min(node_t* node) noexcept {
         while (node->left)
             node = node->left;
@@ -105,13 +114,13 @@ class avl_node_gt {
      */
     template <typename comparable_at>
     static node_t* lower_bound(node_t* node, comparable_at&& comparable) noexcept {
-        node_t* succ = nullptr;
-        auto less = comparator_t {};
+        node_t* successor = nullptr;
+        comparator_t less;
         while (node) {
             // If the given key is less than the root node, visit the left
             // subtree, taking current node as potential successor.
             if (less(comparable, node->entry)) {
-                succ = node;
+                successor = node;
                 node = node->left;
             }
 
@@ -124,11 +133,11 @@ class avl_node_gt {
             // If a node with the desired value is found, the successor is the
             // minimum value node in its right subtree (if any).
             else {
-                succ = node;
-                node = nullptr;
+                successor = node;
+                node = node->left;
             }
         }
-        return succ;
+        return successor;
     }
 
     /**
@@ -143,13 +152,13 @@ class avl_node_gt {
      */
     template <typename comparable_at>
     static node_t* upper_bound(node_t* node, comparable_at&& comparable) noexcept {
-        node_t* succ = nullptr;
-        auto less = comparator_t {};
+        node_t* successor = nullptr;
+        comparator_t less;
         while (node) {
             // If the given key is less than the root node, visit the left
             // subtree, taking current node as potential successor.
             if (less(comparable, node->entry)) {
-                succ = node;
+                successor = node;
                 node = node->left;
             }
 
@@ -163,11 +172,11 @@ class avl_node_gt {
             // minimum value node in its right subtree (if any).
             else {
                 if (node->right)
-                    succ = find_min(node->right);
-                break;
+                    successor = find_min(node->right);
+                node = nullptr;
             }
         }
-        return succ;
+        return successor;
     }
 
     /**
@@ -468,8 +477,10 @@ class avl_node_gt {
             midpoint = downstream.extracted.release();
             midpoint->left = node->left;
             midpoint->right = downstream.root;
+            midpoint->height = 1 + std::max(get_height(midpoint->left), get_height(midpoint->right));
             // Detach the `node` from the descendants.
             node->left = node->right = nullptr;
+            node->height = 1;
             return {midpoint, std::unique_ptr<node_t> {node}};
         }
         // Just one child is present, so it is the natural successor.
@@ -477,12 +488,14 @@ class avl_node_gt {
             node_t* replacement = node->left ? node->left : node->right;
             // Detach the `node` from the descendants.
             node->left = node->right = nullptr;
+            node->height = 1;
             return {replacement, std::unique_ptr<node_t> {node}};
         }
         // No children are present.
         else {
             // Detach the `node` from the descendants.
             node->left = node->right = nullptr;
+            node->height = 1;
             return {nullptr, std::unique_ptr<node_t> {node}};
         }
     }
@@ -559,10 +572,18 @@ class avl_tree_gt {
 
     ~avl_tree_gt() { clear(); }
     std::size_t size() const noexcept { return size_; }
+    std::size_t height() noexcept { return root_ ? root_->height : 0; }
     node_t* root() const noexcept { return root_; }
     node_t* end() const noexcept { return nullptr; }
     node_allocator_t& allocator() noexcept { return allocator_; }
     node_allocator_t const& allocator() const noexcept { return allocator_; }
+
+    std::size_t total_imbalance() const noexcept {
+        std::size_t abs_sum = 0;
+        node_t::for_each_top_down(root_,
+                                  [&](node_t* node) noexcept { abs_sum += std::abs(node_t::get_balance(node)); });
+        return abs_sum;
+    }
 
     template <typename comparable_at>
     node_t* find(comparable_at&& comparable) noexcept {
@@ -672,6 +693,8 @@ class avl_tree_gt {
             root_ = result.root;
             size_ += result.inserted;
         });
+        other.root_ = nullptr;
+        other.size_ = 0;
     }
 
     void merge(extract_result_t other) noexcept {
@@ -967,19 +990,20 @@ class consistent_avl_gt {
 
     void unmask_and_compact(identifier_t const& id, generation_t generation_to_unmask) noexcept {
         // This is similar to the public `erase_range()`, but adds generation-matching conditions.
-        auto last = entries_.lower_bound(id);
+        auto current = entries_.lower_bound(id);
         auto less = entry_comparator_t {};
-        auto last_visible_entry = std::optional<identifier_t> {};
-        while (last && less.same(id, last->entry)) {
-            auto next = entries_.upper_bound(last->entry);
-            last->entry.visible |= last->entry.generation == generation_to_unmask;
-            if (!last->entry.visible)
+        auto last_visible_entry = std::optional<dated_identifier_t> {};
+        while (current && less.same(id, current->entry.element)) {
+            auto next = entries_.upper_bound(current->entry);
+            current->entry.visible |= current->entry.generation == generation_to_unmask;
+            if (!current->entry.visible)
                 continue;
 
             // Older revisions must die
             if (last_visible_entry)
                 entries_.extract(*last_visible_entry);
-            last = next;
+            last_visible_entry = dated_identifier_t {id, current->entry.generation};
+            current = next;
         }
     }
 
@@ -1077,7 +1101,9 @@ class consistent_avl_gt {
 
             // Remove older revisions
             identifier_t id {entry.element};
-            erase_range(id, dated_identifier_t {id, generation});
+            auto status = erase_range(id, dated_identifier_t {id, generation});
+            if (!status)
+                return status;
 
             // Update state for next loop cycle
             last_node = prev_node;
@@ -1218,6 +1244,17 @@ class consistent_avl_gt {
         entries_.clear();
         generation_ = 0;
         return {success_k};
+    }
+
+    template <typename dont_instantiate_me_at>
+    void print(dont_instantiate_me_at& cout) {
+        cout << "Items: " << entries_.size() << std::endl;
+        cout << "Imbalance: " << entries_.total_imbalance() << std::endl;
+        entry_node_t::for_each_left_right(entries_.root(), [&](entry_node_t* node) {
+            char const* marker = node->entry.visible ? "✓" : "✗";
+            cout << identifier_t {node->entry.element} << " @" << node->entry.generation << marker << " ";
+        });
+        cout << std::endl;
     }
 };
 
