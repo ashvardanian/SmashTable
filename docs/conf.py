9318be08feb7a40a131018d6616e65bd5198697a
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-04-23 16:46:22 -0700

    Make: CMake lib and docs
    A top-level `CMakeLists.txt` declares the library as an INTERFACE target with its include
    directory attached, so a consumer links it rather than copying paths.
    
    The example and the test move to the repository root, and the per-example CMake file that
    duplicated the flags is gone.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ extensions = [
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*.md']
 
 
-googleanalytics_id = '341385789'
+googleanalytics_id = 'UA-150644745-1'
 googleanalytics_enabled = True
 
 # -- Options for HTML output -------------------------------------------------
@@ -40,8 +40,12 @@ googleanalytics_enabled = True
 html_logo = '../assets/unum.png'
 html_theme = 'furo'
 html_static_path = ['_static']
-html_css_files = ['custom.css']
-html_js_files = ['custom.js']
+html_css_files = [
+    'custom.css'
+]
+html_js_files = [
+    'custom.js'
+]
 
 
 breathe_projects = {'UCSet': '../build/xml'}
